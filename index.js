2d1d1a08c8e0cce1d4cf3d3c9a299ae4901fec1c
tiagojuliano <tiagojulianodesouza@gmail.com>
2025-11-24 13:35:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 });
 
 // Inicia o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
